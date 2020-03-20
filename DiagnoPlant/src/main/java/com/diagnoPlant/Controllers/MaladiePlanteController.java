d324e396604a61a1a2bab43d47f8cf1af75b4737
zkitilahcen-cyber <atdheserveur@gmail.com>
2020-03-21 00:34:42 +0100

    listeMaladie{pagination et recherche}

--- DiagnoPlant/src/main/java/com/diagnoPlant/Controllers/MaladiePlanteController.java
+++ DiagnoPlant/src/main/java/com/diagnoPlant/Controllers/MaladiePlanteController.java
@@ -1,8 +1,9 @@
 package com.diagnoPlant.Controllers;
 
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,9 +20,17 @@ public class MaladiePlanteController {
 	MaladiePlanteRepository maladiePRepo;
 	
 	@RequestMapping(value="/index")
-	public String Index(Model model) {
-		List<MaladiePlante> mp = maladiePRepo.findAll();
-		model.addAttribute("maladiePlante", mp);
+	public String Index(Model model,
+			@RequestParam(name="page",defaultValue="0")int p ,
+			@RequestParam(name="size", defaultValue="5")int size,
+			@RequestParam(name="chercher", defaultValue="")String chercher) {
+		Page<MaladiePlante> pageMaladiePlante = maladiePRepo
+				.findAll(PageRequest.of(p, size));
+		model.addAttribute("pageMaladiePlante", pageMaladiePlante);
+		model.addAttribute("currentPage",p);
+		model.addAttribute("size",size);
+		model.addAttribute("chercher",chercher);
+		model.addAttribute("pages", new int[pageMaladiePlante.getTotalPages()]);
 		return "listMaladie";
 	}
 	
--- DiagnoPlant/src/main/java/com/diagnoPlant/Repositorys/MaladiePlanteRepository.java
+++ DiagnoPlant/src/main/java/com/diagnoPlant/Repositorys/MaladiePlanteRepository.java
@@ -1,16 +1,17 @@
 package com.diagnoPlant.Repositorys;
 
-
-import java.util.List;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.diagnoPlant.Models.MaladiePlante;
+
+//import org.springframework.data.domain.Page;
+//import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.data.jpa.repository.Query;
+//import org.springframework.data.repository.query.Param;
+
 
 public interface MaladiePlanteRepository extends JpaRepository<MaladiePlante, Long> {
 
-	List<MaladiePlante> findByNomMaladie(String n);
-
-
-
+//	
+//	@Query("select m from MaladiePlante m where m.nomMaladie like :x")
+//	public Page<MaladiePlante> findByDesignationContains(@Param("x")String chercher, Pageable pageable);
 }

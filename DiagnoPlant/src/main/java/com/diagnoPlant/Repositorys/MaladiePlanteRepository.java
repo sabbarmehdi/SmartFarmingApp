d3adc8808445d8703134ac2ae63ceff95e8a8b16
zkitilahcen-cyber <atdheserveur@gmail.com>
2020-03-21 01:30:30 +0100

    organisation_v_1{image}

--- DiagnoPlant/src/main/java/com/diagnoPlant/Repositorys/MaladiePlanteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.diagnoPlant.Repositorys;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.diagnoPlant.Models.MaladiePlante;
-
-public interface MaladiePlanteRepository extends JpaRepository<MaladiePlante,Long>{ 
-
-}

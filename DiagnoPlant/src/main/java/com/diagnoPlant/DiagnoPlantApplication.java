1f1646f2d6f88d6bbd4693659961ef675a3c75b1
zkitilahcen-cyber <atdheserveur@gmail.com>
2020-03-21 01:23:09 +0100

    organisation_v_1{listMaladiePlante}

--- DiagnoPlant/src/main/java/com/diagnoPlant/DiagnoPlantApplication.java
+++ DiagnoPlant/src/main/java/com/diagnoPlant/DiagnoPlantApplication.java
@@ -3,12 +3,9 @@ package com.diagnoPlant;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 
-import com.diagnoPlant.Services.StorageProperties;
 @SpringBootApplication
-@EnableConfigurationProperties(StorageProperties.class)
 public class DiagnoPlantApplication {
 
 	public static void main(String[] args) {

90bbe3fde0348d2842413d7cf65b9f3d8ec32ffe
moubaki <57689622+moubaki@users.noreply.github.com>
2020-03-21 17:30:22 +0100

    organisation V1

--- DiagnoPlant/src/main/java/com/diagnoPlant/main/DiagnoPlantApplication.java
+++ DiagnoPlant/src/main/java/com/diagnoPlant/main/DiagnoPlantApplication.java
@@ -1,28 +1,18 @@
 package com.diagnoPlant.main;
 
-import org.springframework.boot.CommandLineRunner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
 
-import com.diagnoPlant.Services.StorageProperties;
-import com.diagnoPlant.Services.StorageService;
 
 @SpringBootApplication
-@EnableConfigurationProperties(StorageProperties.class)
+
 public class DiagnoPlantApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DiagnoPlantApplication.class, args);
 	}
 	
-	@Bean
-	CommandLineRunner init(StorageService storageService) {
-		return (args) -> {
-			storageService.deleteAll();
-			storageService.init();
-		};
-	}
+	
 
 }

29fc895703054abd26d2d8dafad38bfd9cb37b5e
zkitilahcen-cyber <atdheserveur@gmail.com>
2020-03-22 21:59:47 +0100

    Ok

--- DiagnoPlant/src/main/java/com/diagnoPlant/Controllers/MaladiePlanteController.java
+++ DiagnoPlant/src/main/java/com/diagnoPlant/Controllers/MaladiePlanteController.java
@@ -6,12 +6,18 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.diagnoPlant.Models.MaladiePlante;
 import com.diagnoPlant.Repositorys.MaladiePlanteRepository;
 
+/**
+ * This class used to manage the plant disease list
+ * @author lahcen
+ *
+ */
 
 @Controller
 @RequestMapping(value="/maladiePlantes")
@@ -19,33 +25,38 @@ public class MaladiePlanteController {
 	@Autowired
 	MaladiePlanteRepository maladiePRepo;
 	
-	@RequestMapping(value="/index")
+	/**
+	 * This method get you all plant disease by name
+	 * @author lahcen
+	 * @param Chercher
+	 * @param page
+	 * @param size
+	 */
+	@GetMapping(value="/index")
 	public String Index(Model model,
-			@RequestParam(name="page",defaultValue="0")int p ,
+			@RequestParam(name="page",defaultValue="0")int page ,
 			@RequestParam(name="size", defaultValue="5")int size,
-			@RequestParam(name="chercher", defaultValue="")String chercher) {
-		Page<MaladiePlante> pageMaladiePlante = maladiePRepo
-				.findAll(PageRequest.of(p, size));
+			@RequestParam(name="Chercher", defaultValue="")String Chercher) {
+		Page<MaladiePlante> pageMaladiePlante =maladiePRepo
+				.findByNomMaladieContains(Chercher, PageRequest.of(page, size));
 		model.addAttribute("pageMaladiePlante", pageMaladiePlante);
-		model.addAttribute("currentPage",p);
+		model.addAttribute("currentPage",page);
 		model.addAttribute("size",size);
-		model.addAttribute("chercher",chercher);
+		model.addAttribute("Chercher",Chercher);
 		model.addAttribute("pages", new int[pageMaladiePlante.getTotalPages()]);
 		return "listMaladie";
 	}
 	
-	@RequestMapping(value="/info")
+	/**
+	 * This method get you the info of plant disease
+	 * @author lahcen
+	 * @param idMaladiePlante
+	 * 
+	 */
+	@GetMapping(value="/info")
 	public String Info(Model model, @RequestParam(value="id")Long idMaladiePlante ) {
 		MaladiePlante mpl = maladiePRepo.getOne(idMaladiePlante);
 		model.addAttribute("infoMaladieP", mpl);
 		return "listResponseNamMal";
 	}
-	
-	
-	
-	
-
-
-	
-
 }
--- DiagnoPlant/src/main/java/com/diagnoPlant/Repositorys/MaladiePlanteRepository.java
+++ DiagnoPlant/src/main/java/com/diagnoPlant/Repositorys/MaladiePlanteRepository.java
@@ -2,16 +2,23 @@ package com.diagnoPlant.Repositorys;
 
 import com.diagnoPlant.Models.MaladiePlante;
 
-//import org.springframework.data.domain.Page;
-//import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.data.jpa.repository.Query;
-//import org.springframework.data.repository.query.Param;
 
 
+/**
+ * This class extends all methods of JpaRepository
+ * @author lahcen
+ *
+ */
 public interface MaladiePlanteRepository extends JpaRepository<MaladiePlante, Long> {
-
-//	
-//	@Query("select m from MaladiePlante m where m.nomMaladie like :x")
-//	public Page<MaladiePlante> findByDesignationContains(@Param("x")String chercher, Pageable pageable);
+	
+	/**
+	 * With this method you can search by name(nomMaladie)
+	 * @author lahcen
+	 * @param Chercher
+	 * @param pageable
+	 */
+	public Page<MaladiePlante> findByNomMaladieContains(String Chercher, Pageable pageable);
 }
